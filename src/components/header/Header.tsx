492124772006f2e45be577a7045a2a70ddb756bf
IvanStark <104026643+AzrealStillCute@users.noreply.github.com>
2023-12-20 15:56:04 +0630

    fetch weather

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,61 @@ import Image from "next/image";
 import { Button, LoginBtn } from "../ui/Button";
 import Link from "next/link";
 import SearchBar from "../ui/SearchBar";
-import { Menu } from "lucide-react";
+import { Cloud, List, Sun } from "@phosphor-icons/react/dist/ssr";
 
-function Header() {
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth();
+const day = currentDate.getDay();
+const date = currentDate.getDate();
+
+async function getWeatherData() {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=16.8409&lon=96.199379&units=metric&appid=28d643b869a136f535aedb13963224cb`
+  );
+  const result = await response.json();
+
+  return result;
+}
+
+const weatherIcons = {
+  "01d": <Sun size={22} />,
+  "04d": <Cloud size={22} />,
+};
+
+async function Header() {
+  const weatherData = await getWeatherData();
+  const iconId = weatherData.weather[0].icon;
+
   return (
     <header>
-      <div className="h-12 px-3 border-b border-b-neutral-300 flex items-center justify-between">
+      <div className="h-12 px-3 border-b border-b-neutral-200 flex items-center justify-between">
         <div className="flex items-center gap-x-5 text-neutral-900 ">
           <span className="cursor-pointer">EN</span>
         </div>
@@ -16,15 +65,21 @@ function Header() {
           <Button>Subscribe</Button>
         </div>
       </div>
-      <div className=" grid grid-cols-3 items-center py-10 px-5">
+      <div className=" grid grid-cols-3 items-center py-8 px-5">
         <div className="justify-self-start">
-          <h5 className=" text-sm text-neutral-500">Sunday, December, 2023</h5>
+          <div className=" flex items-center gap-x-2 mb-1">
+            {weatherIcons[iconId as keyof typeof weatherIcons]}
+            <span>{weatherData.main.temp}°C</span>
+            <span className="text-neutral-200">|</span>
+            {weatherData.name}
+          </div>
+          <h5 className=" text-sm text-neutral-500">{`${days[day]}, ${months[month]} ${date}, ${year}`}</h5>
         </div>
         <div className=" justify-self-center">
           <Link href={"/"}>
             <Image
               alt="header_logo"
-              src={"/logo.png"}
+              src={"/logo.svg"}
               height={80}
               width={254}
             />
@@ -41,15 +96,19 @@ function Header() {
 
 function NavBar() {
   return (
-    <div className="grid grid-cols-3 items-center h-12 px-5 border-t border-t-neutral-300 border-b-2 border-b-neutral-800">
+    <div className="grid grid-cols-3 items-center h-12 px-5 border-t border-t-neutral-200 border-b-2 border-b-neutral-800">
       <button className=" flex items-center gap-x-1 text-neutral-800 hover:text-neutral-900">
-        <Menu strokeWidth={1.5} size={20} />
+        <List size={20} />
         <span className="text-sm">More sections</span>
       </button>
       <nav className=" justify-self-center">
         <ul className=" flex gap-x-10">
           {["News", "Sports", "Politics", "Health", "Tech"].map((item) => {
-            return (<Link href={"/"}><li className="text-sm text-neutral-500">{item}</li></Link>);
+            return (
+              <Link href={"/"}>
+                <li className="text-sm text-neutral-500">{item}</li>
+              </Link>
+            );
           })}
         </ul>
       </nav>
